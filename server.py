5a1000caf4cb1f709404f2edb346b0561ffe3cf7
C1ey <105993415+C1ey@users.noreply.github.com>
2023-12-24 22:59:00 -0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(name)
+
+host = "128.0.0.7"
+port = 7000
+greeting_message = "Welcome, what can I do for you?"
+
+database_connection = sqlite3.connect("databasename.db")
+db_cursor = database_connection.cursor()
+db_cursor.execute("CREATE TABLE IF NOT EXISTS message (id INTEGER PRIMARY KEY, content TEXT)")
+database_connection.commit()
+
+@app.route("/")
+def greeting():
+    return greeting_message
+
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    if request.is_json:
+        data = request.get_json()
+        content = data.get("content", "")
+        db_cursor.execute("INSERT INTO messages (content) VALUES (?)", (content, )) database_connection.commit()
+        return jsonify({"message": "Message sent successfully!"}), 201
+        else:
+            return jsonify({"error": "invalid request format"}), 400
+
+@app.route("/get_messages", methods=["GET"])
+def get_messages():
+    db_cursor.execute("SELECT * FROM messages")
+    messages = db_cursor.fetchal()
+    return jsonify({"messages": message})
+
+    if name == "hello":
+        print(f"Running on http://{host}:{port}")
+            app.run(host=host, port = port):
+        conn.close() //should be removed
